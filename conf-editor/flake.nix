1e0e795c6c295f3b9b40c0f41b7411d60e0b4e45
awfixer <git@awfixer.me>
2025-12-28 15:12:29 -0800

    add conf-editor

---
+++ conf-editor/flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+  };
+
+  outputs = inputs:
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
+      alias.packages.default = "nixos-conf-editor";
+      src = ./.;
+    };
+}
